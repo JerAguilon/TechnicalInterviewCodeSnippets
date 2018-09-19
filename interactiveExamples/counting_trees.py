e1f480d2d093d1f376e6ff7b6aa38ead66d2cf3e
Jeremy Aguilon <jeraguilon@gmail.com>
2018-09-18 20:04:24 -0400

    Add another question

---
+++ interactiveExamples/counting_trees.py
@@ -0,0 +1,6 @@
+# Given an int n, return the number of possible trees with n nodes you can construct
+# Example: n = 1 --> 1, the single node
+#          n = 2 --> 2, a parent node with a left child and a parent node with a right child
+
+def solution(n):
+    pass
