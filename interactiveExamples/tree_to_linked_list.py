fdb993cd88da6b86468c7248d0b66890572bc03f
Jeremy Aguilon <jeraguilon@gmail.com>
2018-09-18 20:01:00 -0400

    Add linked list question

---
+++ interactiveExamples/tree_to_linked_list.py
@@ -0,0 +1,12 @@
+# Given the root `Node` of a tree, convert the tree into a double linked list.
+# We don't particularly care about the order, but the linked list should contain
+# all the nodes inside the tree.
+
+class Node(object):
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def solution(root):
+    pass
