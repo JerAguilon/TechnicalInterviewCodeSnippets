e7a8d7e1cfa8de3b880c46025df88b6f40c4c0da
Jeremy Aguilon <jeraguilon@gmail.com>
2019-04-22 23:07:45 -0400

    Improvecomments on sliding window

--- slidingWindow/slidingwindow.py
+++ slidingWindow/slidingwindow.py
@@ -1,29 +1,44 @@
-def solution(string, charSet):
-    left = 0;
-    right = 0;
-    letterMap = {}
-    charactersEncountered = 0;
-    bestScore = float('inf')
+'''
+Given a string and a set of characters, return the **smallest** substring that contains all of the characters in the set
+'''
+
+def solution(string, char_set):
+    # Step 1 and 2
+    left, right, best_score = 0, 0, float('inf')
+
+    # Step 3a:  Generally, for sliding window, you often need a set or hashmap to track
+    #           the characters/values you have in your substring/subarray
+    # Step 3b: This is an auxiliary value that lets us cleanly look up the characters
+    #          in char_set that we have found
+    letter_map = {} # maps from character to number of occurences in the substring
+    characters_encountered = 0 # when this is equal to len(char_set), we have a
+                               # candidate substring
+    # Step 4
     while right < len(string):
         curr_right = string[right]
-        if curr_right in charSet:
-            letterMap[curr_right] =  letterMap.get(curr_right, 0) + 1
-            if letterMap[string[right]] == 1:
-                charactersEncountered += 1
+
+        # Step 5
+        if curr_right in char_set:
+            letter_map[curr_right] =  letter_map.get(curr_right, 0) + 1
+            if letter_map[curr_right] == 1:
+                characters_encountered += 1
         right += 1
-        # we've encountered every letter, let's minimize!
-        if (charactersEncountered == len(charSet)):
-            # make the string 'invalid' again, and update
-            # bestScorebefore continuing
-            while (charactersEncountered == len(charSet)):
+
+        # Step 6: If you have a new candidate substring (in this case we found all
+        #    our letters, begin incrementing left until it is *invalid*
+        if (characters_encountered == len(char_set)):
+            while characters_encountered == len(char_set):
                 curr_left = string[left]
-                if curr_left in charSet:
-                    letterMap[curr_left] -= 1
-                    if letterMap[curr_left] == 0:
-                        charactersEncountered -= 1
+                if curr_left in char_set:
+                    letter_map[curr_left] -= 1
+                    if letter_map[curr_left] == 0:
+                        characters_encountered -= 1
                 left += 1
-            bestScore = min(bestScore, right - left + 1)
-    return bestScore
+            # Step 7: Finally! Update the best score if we have a new best.
+            #    This new candidate substring is bounded by right - left + 1. Avoid
+            #    off-by-one's by drawing an example out.
+            best_score = min(best_score, right - left + 1)
+    return best_score
 
 
 print(solution("adddddbcbba", {'a','b','c'})) # should be 4
