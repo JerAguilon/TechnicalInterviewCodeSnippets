3deef623190790e3bd9754ef9bbd3b1bc22abf7d
Ryan Lewkowicz <ryan.m.lewkowicz@gmail.com>
2020-02-24 07:08:26 -0500

    fixed x,y invert

--- countingIslandsProblem/dfsSolution.py
+++ countingIslandsProblem/dfsSolution.py
@@ -1,19 +1,18 @@
 def solution(arr):
     total = 0
-
     for y in range(len(arr)):
         for x in range(len(arr[y])):
-            if arr[x][y] == 1:
+            if arr[y][x] == 1:
                 total += 1
                 dfs(x,y,arr)
     return total
 
 def dfs(x, y, arr):
-    if x < 0 or x >= len(arr) or y < 0 or y >= len(arr[x]):
+    if y < 0 or y >= len(arr) or x < 0 or x >= len(arr[y]):
         return
-    if arr[x][y] == 0:
+    if arr[y][x] == 0:
         return
-    arr[x][y] = 0
+    arr[y][x] = 0
     dfs(x+1, y, arr)
     dfs(x-1, y, arr)
     dfs(x, y-1, arr)
